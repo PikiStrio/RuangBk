4751de24c7fdc6dcf777db1415f3fd6d956eedf4
Muhammad Vicky Satrio <vickysatrio12@gmail.com>
2024-12-26 16:21:46 +0700

    card career

---
+++ resources/views/components/card-md.blade.php
@@ -0,0 +1,14 @@
+@props(['images', 'title', 'description'])
+
+<div class="w-full sm:basis-1/2 basis-full lg:basis-1/3 p-5">
+    <div class="max-w-sm rounded-xl overflow-hidden shadow-lg mx-auto bg-white">
+        <img class="rounded-full w-24 h-24 object-cover mt-5 ml-5"
+            src="{{ asset('build/assets/images/' . $images . '.png') }}" alt="Sunset in the mountains">
+        <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">{{ $title }}</div>
+            <p class="text-gray-700 text-base">{{ $description }}
+            </p>
+        </div>
+        <a href="" class="py-3 px-7 my-8 mx-auto max-w-xs bg-gray-300 text-gray-800 rounded-full block text-xl text-center font-semibold">View Details</a>
+    </div>
+</div>
